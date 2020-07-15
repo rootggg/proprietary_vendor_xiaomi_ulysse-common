2cf0f4eb39eb6f63b8042309edec08e118e8c477
zqm64 <zqm64@outlook.com>
2020-07-15 21:34:14 +0800

    ugg: fp: Import FPC blobs from stock ugg

--- ugg/ugg-vendor-fingerprint.mk
+++ ugg/ugg-vendor-fingerprint.mk
@@ -13,7 +13,12 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/ugg/fingerprint/vendor/etc/permissions/com.fingerprints.extension.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.fingerprints.extension.xml \
     vendor/xiaomi/ugg/fingerprint/vendor/framework/com.fingerprints.extension.jar:$(TARGET_COPY_OUT_VENDOR)/framework/com.fingerprints.extension.jar \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/com.fingerprints.extension@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.fingerprints.extension@1.0.so \
-    vendor/xiaomi/ugg/fingerprint/vendor/lib64/hw/fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.default.so \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.fingerprint@1.0.so \
     vendor/xiaomi/ugg/fingerprint/vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.fingerprint@1.0.so
+
+# FPC from ugg-user 7.1.2 N2G47H V10.2.1.0.NDKMIXM
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ugg/fingerprint/vendor/lib/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_fpc_tac_shared.so \
+    vendor/xiaomi/ugg/fingerprint/vendor/lib64/lib_fpc_tac_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib64/lib_fpc_tac_shared.so \
+    vendor/xiaomi/ugg/fingerprint/vendor/lib64/hw/fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.default.so
