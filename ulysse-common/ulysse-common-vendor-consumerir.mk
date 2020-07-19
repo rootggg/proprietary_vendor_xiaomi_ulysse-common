33fc155cb187afa2dad66bd63143949bcc8b3e12
zqm64 <zqm64@outlook.com>
2020-07-20 02:13:28 +0800

    ulysse: Unify!

--- ulysse-common/ulysse-common-vendor-consumerir.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/ulysse-common/consumerir/vendor/lib64/hw/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/consumerir.default.so
---
+++ ulysse-vendor-consumerir.mk
@@ -0,0 +1,2 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse/consumerir/vendor/lib64/hw/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/consumerir.default.so
