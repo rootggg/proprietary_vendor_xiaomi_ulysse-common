93fc6c4163e1ca892b87e60e632bd695995150b1
zqm64 <zqm64@outlook.com>
2020-07-22 14:53:07 +0800

    ulysse-common: Go back to be a common tree

---
+++ ulysse-common-vendor-gpu_firmware.mk
@@ -0,0 +1,15 @@
+# Adreno 5xx - from Daisy QKQ1.191002.002 V11.0.5.0.QDLMIXM
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a506_zap.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b00 \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a506_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b01 \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a506_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.b02 \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a506_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a506_zap.mdt \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a530_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pfp.fw \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a530_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pm4.fw
+
+# Adreno 3xx - from X00RD QKQ1.191002.002 17.07.2003.405-20200330
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a300_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pfp.fw \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a300_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pm4.fw \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a330_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a330_pfp.fw \
+    vendor/xiaomi/ulysse-common/gpu_firmware/vendor/firmware/a330_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a330_pm4.fw
--- ulysse-vendor-ril.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/ulysse/ril/vendor/bin/hw/qcrild:$(TARGET_COPY_OUT_VENDOR)/bin/hw/qcrild \
-    vendor/xiaomi/ulysse/ril/vendor/etc/init/qcrild.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/qcrild.rc \
-    vendor/xiaomi/ulysse/ril/vendor/lib64/libqcrilFramework.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqcrilFramework.so \
-    vendor/xiaomi/ulysse/ril/vendor/lib64/libril-qc-hal-qmi.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril-qc-hal-qmi.so \
-    vendor/xiaomi/ulysse/ril/vendor/lib64/qcrild_librilutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/qcrild_librilutils.so
