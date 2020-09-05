bfaefacde70a8d7eabb68f1bb0bf6721400ba777
zqm64 <zqm64@outlook.com>
2020-09-05 16:42:09 +0800

    ulysse-common: Add prebuilt system copy of libbinder and libcutils to lib/vndk

---
+++ ulysse-common-vendor-vndk.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse-common/vndk/vendor/lib/vndk/libbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib/vndk/libbinder.so \
+    vendor/xiaomi/ulysse-common/vndk/vendor/lib/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/vndk-sp/libcutils.so
