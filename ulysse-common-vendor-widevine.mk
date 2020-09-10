ecb72493143f74c24bb8c7077586b0197d54ea70
zqm64 <zqm64@outlook.com>
2020-09-10 20:28:15 +0800

    ulysse-common: Fragmentize Widevine blobs

---
+++ ulysse-common-vendor-widevine.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse-common/widevine/vendor/bin/hw/android.hardware.drm@1.2-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.2-service.widevine \
+    vendor/xiaomi/ulysse-common/widevine/vendor/etc/init/android.hardware.drm@1.2-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.2-service.widevine.rc \
+    vendor/xiaomi/ulysse-common/widevine/vendor/lib/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libwvdrmengine.so \
+    vendor/xiaomi/ulysse-common/widevine/vendor/lib64/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwvhidl.so \
+    vendor/xiaomi/ulysse-common/widevine/vendor/lib64/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib64/mediadrm/libwvdrmengine.so
