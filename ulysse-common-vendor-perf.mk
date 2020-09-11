fbf75770ceeffd6e0f9d472f5231c7f60ef81ded
zqm64 <zqm64@outlook.com>
2020-09-12 02:04:30 +0800

    ulysse-common: perf: Drop energy-awareness
    * See Also: ulysse-common: proprietary-files: remove energy awareness daemon

--- ulysse-common-vendor-perf.mk
+++ ulysse-common-vendor-perf.mk
@@ -1,7 +1,6 @@
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/ulysse-common/perf/product/lib/vendor.qti.hardware.perf@2.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib/vendor.qti.hardware.perf@2.0.so \
     vendor/xiaomi/ulysse-common/perf/product/lib64/vendor.qti.hardware.perf@2.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.hardware.perf@2.0.so \
-    vendor/xiaomi/ulysse-common/perf/vendor/bin/energy-awareness:$(TARGET_COPY_OUT_VENDOR)/bin/energy-awareness \
     vendor/xiaomi/ulysse-common/perf/vendor/bin/hw/vendor.qti.hardware.perf@2.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.perf@2.0-service \
     vendor/xiaomi/ulysse-common/perf/vendor/bin/msm_irqbalance:$(TARGET_COPY_OUT_VENDOR)/bin/msm_irqbalance \
     vendor/xiaomi/ulysse-common/perf/vendor/etc/init/vendor.qti.hardware.perf@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf@2.0-service.rc \
