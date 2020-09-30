b3744f9779d8ba369426fed9ea54b0e274879253
zqm64 <zqm64@outlook.com>
2020-09-30 10:05:22 +0800

    ulysse-common: Import ims_rtp 2.1 blobs from LA.UM.8.6.2.r1-08100-89xx.0

---
+++ ulysse-common-vendor-ims_rtp.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/bin/ims_rtp_daemon:${TARGET_COPY_OUT_VENDOR}/bin/ims_rtp_daemon \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/etc/init/ims_rtp_daemon.rc:${TARGET_COPY_OUT_VENDOR}/etc/init/ims_rtp_daemon.rc \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/lib-rtpcommon.so:${TARGET_COPY_OUT_VENDOR}/lib64/lib-rtpcommon.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/lib-rtpcore.so:${TARGET_COPY_OUT_VENDOR}/lib64/lib-rtpcore.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/lib-rtpsl.so:${TARGET_COPY_OUT_VENDOR}/lib64/lib-rtpsl.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/vendor.qti.imsrtpservice@2.0.so:${TARGET_COPY_OUT_VENDOR}/lib64/vendor.qti.imsrtpservice@2.0.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/vendor.qti.imsrtpservice@2.1-service-Impl.so:${TARGET_COPY_OUT_VENDOR}/lib64/vendor.qti.imsrtpservice@2.1-service-Impl.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/vendor/lib64/vendor.qti.imsrtpservice@2.1.so:${TARGET_COPY_OUT_VENDOR}/lib64/vendor.qti.imsrtpservice@2.1.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/product/lib64/vendor.qti.imsrtpservice@2.0.so:${TARGET_COPY_OUT_PRODUCT}/lib64/vendor.qti.imsrtpservice@2.0.so \
+    vendor/xiaomi/ulysse-common/ims_rtp/product/lib64/vendor.qti.imsrtpservice@2.1.so:${TARGET_COPY_OUT_PRODUCT}/lib64/vendor.qti.imsrtpservice@2.1.so
