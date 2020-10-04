7e5a13d2dabb340160a4ce52f11a79bc47d222e9
zqm64 <zqm64@outlook.com>
2020-10-04 17:11:55 +0800

    Revert "ulysse-common: drm: Add vendor.qti.hardware.qseecom@1.0 blobs"
    This reverts commit 17bfcc08b761902a629fac48d6013c6587cfd251.

--- ulysse-common-vendor-drm.mk
+++ ulysse-common-vendor-drm.mk
@@ -13,14 +13,3 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/ulysse-common/drm/vendor/lib64/libsecureui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsecureui.so \
     vendor/xiaomi/ulysse-common/drm/vendor/lib64/libsecureui_svcsock.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsecureui_svcsock.so \
     vendor/xiaomi/ulysse-common/drm/vendor/lib64/libssd.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libssd.so
-
-# vendor.qti.hardware.qseecom@1.0
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/ulysse-common/drm/product/lib64/vendor.qti.hardware.qseecom@1.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/vendor.qti.hardware.qseecom@1.0.so \
-    vendor/xiaomi/ulysse-common/drm/product/lib/vendor.qti.hardware.qseecom@1.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib/vendor.qti.hardware.qseecom@1.0.so \
-    vendor/xiaomi/ulysse-common/drm/vendor/bin/hw/vendor.qti.hardware.qseecom@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.qseecom@1.0-service \
-    vendor/xiaomi/ulysse-common/drm/vendor/etc/init/vendor.qti.hardware.qseecom@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.qseecom@1.0-service.rc \
-    vendor/xiaomi/ulysse-common/drm/vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so \
-    vendor/xiaomi/ulysse-common/drm/vendor/lib64/vendor.qti.hardware.qseecom@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.qseecom@1.0.so \
-    vendor/xiaomi/ulysse-common/drm/vendor/lib/hw/vendor.qti.hardware.qseecom@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vendor.qti.hardware.qseecom@1.0-impl.so \
-    vendor/xiaomi/ulysse-common/drm/vendor/lib/vendor.qti.hardware.qseecom@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.qseecom@1.0.so
