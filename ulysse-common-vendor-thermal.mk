2c6155761f450c91592c0e412f5d3c19b57228a7
zqm64 <zqm64@outlook.com>
2020-10-04 21:53:59 +0800

    Revert "ulysse-common: thermal: Update from LA.UM.8.6.2.r1-08100-89xx.0 msm8937"
    This reverts commit 07c3c25c8cc8af837fde607a2a9aa081597f9820.

--- ulysse-common-vendor-thermal.mk
+++ ulysse-common-vendor-thermal.mk
@@ -1,2 +1,3 @@
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/ulysse-common/thermal/vendor/bin/thermal-engine:$(TARGET_COPY_OUT_VENDOR)/bin/thermal-engine
+    vendor/xiaomi/ulysse-common/thermal/vendor/bin/thermal-engine:$(TARGET_COPY_OUT_VENDOR)/bin/thermal-engine \
+    vendor/xiaomi/ulysse-common/thermal/vendor/lib64/libthermalioctl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libthermalioctl.so
